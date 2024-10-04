62981dd6c8f53ca20743abe5f9338177df128f60
Pooja Konduri <poojakonduru15@gmail.com>
2024-10-04 15:02:17 +0530

    [pooja] Add: to export data form MSSQL

---
+++ export_file.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Define the connection string
+connection_string = "mssql+pyodbc://LAPTOP-77B90P0P\\SQLEXPRESS/test_db?driver=ODBC+Driver+17+for+SQL+Server"
+
+# Create a SQLAlchemy engine
+engine = create_engine(connection_string)
+
+# Query the data into a DataFrame
+df = pd.read_sql("SELECT * FROM dbo.produ", engine)
+
+# Save the DataFrame to a CSV file
+df.to_csv(r"C:\\Users\\pooja konduri\\OneDrive\\Desktop\\BridgeLabs\\MS-SQL\\pro.csv", index=False)
+
+print("Data exported successfully to pro.csv.")
